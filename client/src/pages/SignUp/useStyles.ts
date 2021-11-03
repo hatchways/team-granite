94a974b79f561d26cea09a21255efe5d02a14322
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2021-11-03 14:54:30 -0400

    removed comment

--- client/src/pages/SignUp/useStyles.ts
+++ client/src/pages/SignUp/useStyles.ts
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
-    // minHeight: '100px',
     '& .MuiInput-underline:before': {
       borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
     },
