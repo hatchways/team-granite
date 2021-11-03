5b4f3704b7da77f30e5aae0a90dcd160a7bc3192
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2021-11-03 01:35:43 -0400

    styled login and signup pages

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,15 +1,14 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import { FormikHelpers } from 'formik';
 import Typography from '@material-ui/core/Typography';
 import useStyles from './useStyles';
 import register from '../../helpers/APICalls/register';
-import SignUpForm from './SignUpForm/SignUpForm';
-import AuthHeader from '../../components/AuthHeader/AuthHeader';
 import { useAuth } from '../../context/useAuthContext';
 import { useSnackBar } from '../../context/useSnackbarContext';
+import { Link } from 'react-router-dom';
+import { Divider } from '@material-ui/core';
+import SignUpForm from './SignUpForm/SignUpForm';
 
 export default function Register(): JSX.Element {
   const classes = useStyles();
@@ -30,7 +29,6 @@ export default function Register(): JSX.Element {
       } else {
         // should not get here from backend but this catch is for an unknown issue
         console.error({ data });
-
         setSubmitting(false);
         updateSnackBarMessage('An unexpected error occurred. Please try again');
       }
@@ -38,23 +36,34 @@ export default function Register(): JSX.Element {
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container className={classes.root}>
       <CssBaseline />
-      <Grid item xs={12} sm={8} md={7} elevation={6} component={Paper} square>
-        <Box className={classes.authWrapper}>
-          <AuthHeader linkTo="/login" asideText="Already have an account?" btnText="Login" />
-          <Box width="100%" maxWidth={450} p={3} alignSelf="center">
-            <Grid container>
-              <Grid item xs>
-                <Typography className={classes.welcome} component="h1" variant="h5">
-                  Create an account
-                </Typography>
-              </Grid>
-            </Grid>
-            <SignUpForm handleSubmit={handleSubmit} />
-          </Box>
-          <Box p={1} alignSelf="center" />
-        </Box>
+      <Grid item xs={6}>
+        <img
+          src="https://res.cloudinary.com/dnisjqp90/image/upload/v1635871008/Images/sign-up2_e7xshm.png"
+          className={classes.imageFill}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Grid xs={12} className={`${classes.center} ${classes.welcomeDiv}`}>
+          <Typography className={classes.welcome} component="h1" variant="h5">
+            Sign up to Kanban
+          </Typography>
+        </Grid>
+        <Grid xs={12} className={`${classes.center} ${classes.loginDiv}`}>
+          <SignUpForm handleSubmit={handleSubmit} />
+        </Grid>
+        <Grid xs={12}>
+          <Divider />
+          <Typography className={`${classes.center} ${classes.bottomText}`} component="h1" variant="h5">
+            Already have an account?
+          </Typography>
+          <Link to="/login" className={classes.linkProp}>
+            <Typography className={`${classes.center} ${classes.linkText}`} component="h1" variant="h5">
+              Login
+            </Typography>
+          </Link>
+        </Grid>
       </Grid>
     </Grid>
   );
--- client/src/pages/SignUp/useStyles.ts
+++ client/src/pages/SignUp/useStyles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
-    minHeight: '100vh',
+    // minHeight: '100px',
     '& .MuiInput-underline:before': {
       borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
     },
@@ -16,11 +16,40 @@ const useStyles = makeStyles(() => ({
     paddingTop: 23,
   },
   welcome: {
-    fontSize: 26,
-    paddingBottom: 20,
+    fontSize: 35,
+    paddingBottom: 15,
     color: '#000000',
-    fontWeight: 700,
+    fontWeight: 550,
     fontFamily: "'Open Sans'",
+  },
+  welcomeDiv: {
+    marginTop: 180,
+  },
+  loginDiv: {
+    paddingTop: 50,
+    paddingBottom: 100,
+  },
+  imageFill: {
+    width: '100%',
+    height: '100%',
+  },
+  center: {
+    textAlign: 'center',
+  },
+  bottomText: {
+    fontSize: 17,
+    paddingTop: 50,
+    fontWeight: 1000,
+  },
+  linkText: {
+    fontSize: 17,
+    paddingTop: 10,
+    textDecoration: 'none',
+    fontWeight: 1000,
+    color: '#759CFC',
+  },
+  linkProp: {
+    textDecoration: 'none',
   },
 }));
 
