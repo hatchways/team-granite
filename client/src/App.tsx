a21d1de0531f1c5903c5e7680bcdbdda2c8c1497
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2021-11-07 01:40:57 -0400

    quick fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,9 +20,7 @@ function App(): JSX.Element {
               <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
-                <ProtectedRoute exact path="/dashboard">
-                  <Dashboard />
-                </ProtectedRoute>
+                <ProtectedRoute exact path="/dashboard" component={Dashboard} />
                 <Route path="*">
                   <Redirect to="/login" />
                 </Route>
