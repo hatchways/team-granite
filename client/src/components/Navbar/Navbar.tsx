9790169f7c37f6c83c62616905c2118a656cfcfd
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2021-11-10 10:04:32 -0500

    hotfix to navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = (): JSX.Element => {
   return (
     <Box>
       <Grid container className={classes.container} xs={12}>
-        <Grid item container justifyContent="space-between" alignItems="center" xs={12}>
+        <Grid item container justify="space-between" xs={12}>
           <Grid item xs={4}>
             <Box ml={3}>
               <img src={logoImage} alt="logo" />
@@ -22,9 +22,9 @@ const Navbar = (): JSX.Element => {
 
           <Grid item container alignItems="center" xs={4}>
             <Router>
-              <Grid item container justifyContent="space-evenly" alignItems="center">
+              <Grid item container justify="space-evenly">
                 <NavLink exact to="/page2" className={classes.navNotActive} activeClassName={classes.navActive}>
-                  <Grid item container justifyContent="flex-end" alignItems="center" spacing={1}>
+                  <Grid item container justify="flex-end" spacing={1}>
                     <Grid item>
                       <DashboardIcon />
                     </Grid>
@@ -34,7 +34,7 @@ const Navbar = (): JSX.Element => {
                   </Grid>
                 </NavLink>
                 <NavLink exact to="/page1" className={classes.navNotActive} activeClassName={classes.navActive}>
-                  <Grid item container justifyContent="flex-end" alignItems="center" spacing={1}>
+                  <Grid item container justify="flex-end" spacing={1}>
                     <Grid item>
                       <CalendarTodayIcon />
                     </Grid>
