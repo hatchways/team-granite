5b40eaa9067bc4aa6e73f57d295e179a4d0eeb80
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2021-11-14 16:36:31 -0500

    changes

--- server/models/plugins/cardAttachment.js
+++ server/models/plugins/cardAttachment.js
@@ -6,36 +6,63 @@ const cardAttachmentSchema = new mongoose.Schema({
   files: [{ type: ObjectId, ref: "file", default: [] }],
 });
 
-cardAttachmentSchema.methods.get = async (pluginId) => {
-  const plugin = await this.findById(pluginId);
-  return plugin.files;
+cardAttachmentSchema.methods.get = async ({ body, params, query }) => {
+  return { response: 200, data: { success: this.files } };
 };
 
-cardAttachmentSchema.methods.create = async (pluginId, files) => {
-  const plugin = await this.findById(pluginId);
-  if (plugin.files.length === 0) {
-    plugin.files = files;
-    await plugin.save();
+cardAttachmentSchema.methods.create = async ({ body, params, query }) => {
+  const { files } = body;
+  const { rewrite } = query;
+
+  if (this.files.length === 0 || rewrite) {
+    this.files = files;
+    await this.save();
+    return { response: 201, data: { success: "Files added." } };
   } else {
     throw new Error("Cannot rewrite files.");
   }
 };
 
-cardAttachmentSchema.methods.update = async (pluginId, files) => {
-  const plugin = await this.findbyId(pluginId);
-  files.map((file) => {
-    plugin.files.push(file);
-  });
-  await plugin.save();
+cardAttachmentSchema.methods.update = async ({ body, params, query }) => {
+  const { files, index } = body;
+
+  if (!index) {
+    files.map((file) => {
+      this.files.push(file);
+    });
+  } else {
+    const updatedFiles = Array.from(files);
+    updatedFiles.splice(index, 0, files);
+    this.files = updatedFiles;
+  }
+
+  await this.save();
+
+  return { response: 200, data: { success: "Files updated." } };
 };
 
-cardAttachmentSchema.methods.delete = async (pluginId) => {
-  const plugin = await this.findbyId(pluginId);
-  plugin.files.map(async (file) => {
-    this.model("file").deleteOne(file);
-  });
-  plugin.files = [];
-  await plugin.save();
+cardAttachmentSchema.methods.delete = async ({ body, params, query }) => {
+  const { indices } = body;
+
+  if (!indices) {
+    this.files.map(async (file) => {
+      this.model("file").deleteOne(file);
+    });
+    this.files = [];
+    await this.save();
+  } else {
+    const files = Array.from(this.files);
+    this.files.map(async (file, index) => {
+      if (indices.includes(index)) {
+        this.model("file").deleteOne(file);
+        files.splice(index, 1);
+      }
+    });
+    this.files = files;
+    await this.save();
+  }
+
+  return { response: 200, data: { success: "Files deleted." } };
 };
 
 module.exports = CardAttachment = basePlugin.discriminator(
